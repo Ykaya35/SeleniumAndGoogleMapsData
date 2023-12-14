34cb8094b56b531ea5a7d7fcb885f21c7a539659
Yalçın <Ykdestek35@gmail.com>
2023-12-14 16:55:16 +0300

    Selenium and Google Maps Data
    pulling business data via google map

---
+++ GoogleMapBoot/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace GoogleMapBoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(654, 368);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txt_city
+            // 
+            this.txt_city.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_city.Location = new System.Drawing.Point(6, 19);
+            this.txt_city.Multiline = true;
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(416, 30);
+            this.txt_city.TabIndex = 1;
+            this.txt_city.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_city_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(20, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 118);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Verileri Çek ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_city);
+            this.groupBox1.Location = new System.Drawing.Point(174, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 63);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aranacak kelime Giriniz ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(180, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Aranacak Yeri Temizle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(506, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Tabloyu Temizle ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(365, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 41);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Tablodan Tek Sil";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(621, 49);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(0, 13);
+            this.labelProgress.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.OrangeRed;
+            this.button5.ForeColor = System.Drawing.Color.Transparent;
+            this.button5.Location = new System.Drawing.Point(608, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 52);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 531);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ GoogleMapBoot/Form1.cs
@@ -0,0 +1,235 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium.Interactions;
+using System.Threading.Tasks;
+
+namespace GoogleMapBoot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread th;
+        ChromeDriver drv;
+        private string MapsUrl = "https://www.google.com/maps";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            th = new Thread(loadUrl); th.Start();
+
+        }
+        private void loadUrl()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "İsim";
+            dataGridView1.Columns[1].Name = "Url";
+            dataGridView1.Columns[2].Name = "Tel No";
+            dataGridView1.Columns[3].Name = "Adres";
+            //dataGridView1.Columns[4].Name = "işletme";
+
+            //ChromeOptions options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            drv = new ChromeDriver();
+            drv.Manage().Window.Maximize();
+            drv.Navigate().GoToUrl(MapsUrl);
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            drv.Close();
+        }
+
+        private async void button_Click(object sender, EventArgs e)
+        {
+            //th = new Thread(vericek); th.Start();
+
+            await Task.Run(() =>
+            {
+                vericek();
+            });
+        }
+        private void DataGridViewEkle(string isim, string Url, string tel_no, string Adres)
+        {
+            if (dataGridView1.InvokeRequired)
+            {
+                dataGridView1.Invoke((MethodInvoker)delegate
+                {
+                    dataGridView1.Rows.Add(isim, Url, tel_no, Adres);
+                });
+            }
+            else
+            {
+                dataGridView1.Rows.Add(isim, Url, tel_no, Adres);
+            }
+        }
+
+
+        private void vericek()
+        {
+            WebDriverWait wait = new WebDriverWait(drv, TimeSpan.FromSeconds(10));
+
+            drv.FindElement(By.XPath("//*[@name='q']")).SendKeys(txt_city.Text + OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(4000);
+            // Düğmeyi tanımlayın (Örneğin, düğme XPath veya ID ile tanımlanabilir)
+            IWebElement button = drv.FindElement(By.ClassName("D6NGZc"));
+
+            // Düğmeye tıkla
+            button.Click();
+            int rowCount = 0; // Satır sayacı
+            for (int i = 0; i < drv.FindElements(By.XPath("//*[@class='Nv2PK THOPZb CpccDe ']"))
+                .Count; i++)
+            {
+                try
+                {
+                    drv.FindElements(By.XPath("//*[@class='Nv2PK THOPZb CpccDe ']//*[@class='hfpxzc']"))[i].Click();
+                    Thread.Sleep(4000);
+                }
+                catch
+                {
+                    MessageBox.Show("İstenmeyen bir arama ya da sistemsel bir sorun oluştu. Tekrar deneyiniz.");
+                }
+
+                string isim = "";
+                string Url = "";
+                string tel_no = "";
+                string Adres = "";
+                string isletme = "";
+
+                IWebElement elementName = drv.FindElement(By.XPath("//h1[@class='DUwDvf lfPIob']"));
+                isim = elementName.Text;
+
+                try
+                {
+                    IWebElement elementUrl = drv.FindElement(By.XPath("//*[@data-tooltip='Web sitesini aç']"));
+                    Url = "www." + elementUrl.Text;
+                }
+                catch (NoSuchElementException)
+                {
+                    Url = "Web sitesi yok";
+                }
+
+                try
+                {
+                    IWebElement elementTel = drv.FindElement(By.XPath("//*[@data-tooltip='Telefon numarasını kopyala']"));
+                    tel_no = elementTel.Text;
+                }
+                catch
+                {
+                    tel_no = "Telefon numarası yok";
+                }
+
+                try
+                {
+
+                    WebDriverWait await = new WebDriverWait(drv, TimeSpan.FromSeconds(10));
+                    IWebElement elementAdres = wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@data-tooltip='Adresi kopyala']")));
+                    Adres = elementAdres.Text;
+                }
+                catch
+                {
+                    Adres = "Adres Yok";
+                }
+
+
+                //IWebElement elementisletme = drv.FindElement(By.XPath("//*[@class='DkEaL']"));
+        
+                //isletme = elementisletme.Text;
+                DataGridViewEkle(isim, Url, tel_no, Adres);
+                rowCount++; // Satır sayacını artır
+
+                if (rowCount % 18 == 0)
+                {
+                    Thread.Sleep(5000); // Her 18 satırda 5 saniye beklemek için
+                    scrollCountGeri();
+                }
+
+                scrollCountGo();
+
+            }
+
+            MessageBox.Show("Veriler Çekildi");
+
+        }
+
+        private void UpdateUI(int progress)
+        {
+            // İşlem ilerlemesini GUI üzerinde güncelle
+            labelProgress.Text = "İlerleme: " + progress + "%";
+        }
+
+        private void scrollCountGeri()
+        {
+            var scrollOrigin = new WheelInputDevice.ScrollOrigin
+            {
+                Viewport = true,
+                XOffset = 237,
+                YOffset = 499
+            };
+            new OpenQA.Selenium.Interactions.Actions(drv)
+                .ScrollFromOrigin(scrollOrigin, 0, -300)
+                .Perform();
+        }
+
+        private void scrollCountGo()
+        {
+            var scrollOrigin = new WheelInputDevice.ScrollOrigin
+            {
+                Viewport = true,
+                XOffset = 237,
+                YOffset = 499
+            };
+            new OpenQA.Selenium.Interactions.Actions(drv)
+                .ScrollFromOrigin(scrollOrigin, 0, 200)
+                .Perform();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) /// Tablo Temizle 
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Aranacak alanı temizler 
+        {
+            if (txt_city.Text != null)
+            {
+                txt_city.Clear();
+                drv.FindElement(By.XPath("//*[@aria-label='Kapat']")).Click();
+            }
+        }
+
+        private void txt_city_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                th = new Thread(vericek); th.Start();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dataGridView1.SelectedRows)
+            {
+                if (!item.IsNewRow) // Yeni bir satırı silme
+                {
+                    dataGridView1.Rows.RemoveAt(item.Index);
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            drv.Quit();
+        }
+    }
+}
